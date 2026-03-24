87dfb6b79f396d4c0190d51252adbc31306c567a
Anastasiia <s25453@pjwstk.edu.pl>
2026-03-25 00:52:43 +0100

    Add data

--- Homework3/Program.cs
+++ Homework3/Program.cs
@@ -1,3 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Homework3;
 
-Console.WriteLine("Hello, World!");
+var service = new Service();
+
+var student = new Student("Anastasiia", "Ivanova");
+var employee = new Employee("Jan", "Kowalski");
+var laptop = new Laptop(12.0, "13.6", "Model R", true, "black", "macOS");
+var camera = new Camera("RAW", "45MP", "45MPCanon R5", true, "white", "Canon");
+
+Console.WriteLine($"Users: {student.Name} (ID:{student.Id}), {employee.Name} (ID:{employee.Id})");
+
+service.RentItem(student, laptop);
+
+try {
+    service.RentItem(student, camera); 
+    var projector = new Projector("Epson EB", "black",false,"Epson","4K", "LED");
+    service.RentItem(student, projector); 
+} catch (Exception ex) {
+    Console.WriteLine($"Expected error: {ex.Message}");
+}
+
+service.PrintReport();
